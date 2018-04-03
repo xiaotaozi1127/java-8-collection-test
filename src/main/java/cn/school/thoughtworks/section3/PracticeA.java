4888cc437f8634d496d1d0c47d0ef3285f846308
htao <htao@thoughtworks.com>
2018-04-03 09:02:24 +0800

    [taohui] implement section3 practiceA

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,12 +1,35 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeA {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
+    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> collectionB) {
         //实现练习要求，并改写该行代码。
+        Map<String,Integer> collection2 = new HashMap<String,Integer>(){};
+        List<String> value = new ArrayList<>();
+        collectionB.entrySet().forEach(s -> value.addAll(s.getValue()));
 
-        return null;
+        value.stream().distinct().forEach(i -> {
+            Integer count = value.stream().filter(j -> j == i).collect(Collectors.toList()).size();
+            collection2.put(i, count);
+        });
+
+        Map<String, Integer> output = new HashMap<>();
+        collectionA.entrySet().forEach(s -> {
+            String key = s.getKey();
+            Integer size = s.getValue();
+
+            if(collection2.containsKey(key)){
+                output.put(key, size - Integer.parseInt(collection2.get(key).toString()));
+            }else{
+                output.put(key, size);
+            }
+        });
+
+        return output;
     }
 }
