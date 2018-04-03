f3f3eb0643f3827e268a5b07a6a0035a250c3e92
htao <htao@thoughtworks.com>
2018-04-03 09:10:46 +0800

    [taohui] fix section3 practiceA logic

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -9,22 +9,16 @@ import java.util.stream.Collectors;
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> collectionB) {
         //实现练习要求，并改写该行代码。
-        Map<String,Integer> collection2 = new HashMap<String,Integer>(){};
         List<String> value = new ArrayList<>();
         collectionB.entrySet().forEach(s -> value.addAll(s.getValue()));
-
-        value.stream().distinct().forEach(i -> {
-            Integer count = value.stream().filter(j -> j == i).collect(Collectors.toList()).size();
-            collection2.put(i, count);
-        });
 
         Map<String, Integer> output = new HashMap<>();
         collectionA.entrySet().forEach(s -> {
             String key = s.getKey();
             Integer size = s.getValue();
 
-            if(collection2.containsKey(key)){
-                output.put(key, size - Integer.parseInt(collection2.get(key).toString()));
+            if(value.contains(key)){
+                output.put(key, size - 1);
             }else{
                 output.put(key, size);
             }
